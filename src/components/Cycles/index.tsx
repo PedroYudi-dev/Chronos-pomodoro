443863f1d23996ad4d2e49e01cbc917716cca984
Pedro Yudi <pedro.yudi005@gmail.com>
2025-12-12 17:35:45 -0300

    Construimos o ciclos da forma correta

--- src/components/Cycles/index.tsx
+++ src/components/Cycles/index.tsx
@@ -1,27 +1,38 @@
+import { useTaskContext } from "../../contexts/TaskContext/useTaskContext";
+import { getNextCycle } from "../../Utils/getNextCycle";
+import { getNextCycleType } from "../../Utils/getNextCycleType";
 import styles from "./styles.module.css"
 
 export default function Cycles(){
+
+  const {state} = useTaskContext()
+
+  const arrayCycles = Array.from({length: state.currentCycle})
+
+  const cycleDescripitionMap = {
+    workTime: "Foco",
+    shortBreakTime: "descanso curto",
+    longBreakTime: "descanso longo"  
+  }
+
     return (
       <div className={styles.cycles}>
         <span>Ciclos:</span>
 
         <div className={styles.cyclesDots}>
-          <span className={`${styles.cyclesDot} ${styles.workTime}`}></span>
-          <span
-            className={`${styles.cyclesDot} ${styles.shortBreakTime}`}
-          ></span>
-          <span className={`${styles.cyclesDot} ${styles.workTime}`}></span>
-          <span
-            className={`${styles.cyclesDot} ${styles.shortBreakTime}`}
-          ></span>
-          <span className={`${styles.cyclesDot} ${styles.workTime}`}></span>
-          <span
-            className={`${styles.cyclesDot} ${styles.shortBreakTime}`}
-          ></span>
-          <span className={`${styles.cyclesDot} ${styles.workTime}`}></span>
-          <span
-            className={`${styles.cyclesDot} ${styles.longBreakTime}`}
-          ></span>
+          {arrayCycles.map((value, index) =>{
+            const nextCycle = getNextCycle(index)
+            const nextCycleType = getNextCycleType(nextCycle);
+            return (
+              <span
+                key={`_${nextCycle}`}
+                className={`${styles.cyclesDot} ${styles[nextCycleType]}`}
+                arial-label={`Indicador de ciclos de ${cycleDescripitionMap[nextCycleType]}`}
+                title={`Indicador de ciclos de ${cycleDescripitionMap[nextCycleType]} `}
+              ></span>
+            );
+          })}
+          
         </div>
       </div>
     );
