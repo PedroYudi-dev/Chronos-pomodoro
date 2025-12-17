b9ab1dcdd239f90047f8ca8e78fee5417dcdce96
Pedro Yudi <pedro.yudi005@gmail.com>
2025-12-17 16:19:44 -0300

    Adicionadno audio

--- src/contexts/TaskContext/TaskContextProvaider.tsx
+++ src/contexts/TaskContext/TaskContextProvaider.tsx
@@ -1,9 +1,10 @@
-import { useEffect, useReducer } from "react";
+import { useEffect, useReducer, useRef } from "react";
 import { TaskContext } from "./taskContext";
 import { initiaTasklState } from "./initialTaskState";
 import { taskReducer } from "./taskReducer";
 import { TimerWorkeManager } from "../../workers/TimerWokerManager";
 import { TaskActionType } from "./taskActions";
+import { loadBeep } from "../../Utils/loadBeep";
 
 type TaskContextProvaider = {
   children: React.ReactNode;
@@ -11,6 +12,7 @@ type TaskContextProvaider = {
 
 export function TaskContextProvaider({ children }: TaskContextProvaider) {
   const [state, dispachTask] = useReducer(taskReducer, initiaTasklState);
+  const playBeepRef = useRef<() => void | null>(null)
 
   // AQUI NÓS COSEGUIMOS UTILIZAR O TEMPO SEM ELE PERDER O TEMPO DELE, POR EXEMPLO SE EU ESTOU EM OUTRA TELA SEM SER A DO CONTADORR
   // Ela vai constinuar contando assim ele não fica criando uma nova e sim pausando e voltando no ciclo que esta sendo realizado
@@ -18,8 +20,10 @@ export function TaskContextProvaider({ children }: TaskContextProvaider) {
 
   worker.onmessage((e) => {
     const countDouwnSeconds = e.data
-    console.log(countDouwnSeconds);
     if(countDouwnSeconds <= 0){
+      if(playBeepRef.current){
+        playBeepRef.current()
+      }
       dispachTask({
         type: TaskActionType.COMPLETE_TASK,
       });
@@ -32,13 +36,22 @@ export function TaskContextProvaider({ children }: TaskContextProvaider) {
     }
   });
 
+  // AQUI O ESTADO TODO MUDA 
   useEffect(() => {
     if (!state.activeTask) {
-      console.log("Worker terminado por falta de tarefa");
+      // console.log("Worker terminado por falta de tarefa");
       worker.terminate();
     }
     worker.postMessage(state);
   }, [worker, state]);
+
+  useEffect(() =>{
+    if(state.activeTask && playBeepRef.current === null){
+      playBeepRef.current = loadBeep()
+    }else{
+        playBeepRef.current = null;
+    }
+  }, [state.activeTask])
 
   return (
     <>
