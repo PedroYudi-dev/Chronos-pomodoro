618c8dc180c0c3569efdfc57a6f145592abcffd4
Pedro Yudi <pedro.yudi005@gmail.com>
2025-12-17 12:34:47 -0300

    Foi adicionado no Worker uma contagem para lo cronometro

--- src/contexts/TaskContext/TaskContextProvaider.tsx
+++ src/contexts/TaskContext/TaskContextProvaider.tsx
@@ -2,25 +2,41 @@ import { useEffect, useReducer } from "react";
 import { TaskContext } from "./taskContext";
 import { initiaTasklState } from "./initialTaskState";
 import { taskReducer } from "./taskReducer";
+import { TimerWorkeManager } from "../../workers/TimerWokerManager";
 
-  
 type TaskContextProvaider = {
-     children: React.ReactNode
-}
+  children: React.ReactNode;
+};
 
-export function TaskContextProvaider({ children }: TaskContextProvaider){
-
+export function TaskContextProvaider({ children }: TaskContextProvaider) {
   const [state, dispachTask] = useReducer(taskReducer, initiaTasklState);
 
+  // AQUI NÓS COSEGUIMOS UTILIZAR O TEMPO SEM ELE PERDER O TEMPO DELE, POR EXEMPLO SE EU ESTOU EM OUTRA TELA SEM SER A DO CONTADORR
+  // Ela vai constinuar contando assim ele não fica criando uma nova e sim pausando e voltando no ciclo que esta sendo realizado
+  const worker = TimerWorkeManager.getInstance();
+
+  worker.onmessage((e) => {
+    const countDouwnSeconds = e.data
+    console.log(countDouwnSeconds);
+    if(countDouwnSeconds <= 0){
+      console.log("Worker Finalizado")
+      worker.terminate()
+    }
+  });
+
   useEffect(() => {
-    console.log(state);
-  }, [state]);
-  
-    return (
-      <>
-        <TaskContext.Provider value={{ state, dispachTask }}>
-          {children}
-        </TaskContext.Provider>
-      </>
-    );
+    if (!state.activeTask) {
+      console.log("Worker terminado por falta de tarefa");
+      worker.terminate();
+    }
+    worker.postMessage(state);
+  }, [worker, state]);
+
+  return (
+    <>
+      <TaskContext.Provider value={{ state, dispachTask }}>
+        {children}
+      </TaskContext.Provider>
+    </>
+  );
 }
---
+++ src/workers/TimerWokerManager.ts
@@ -0,0 +1,34 @@
+let  instance: TimerWorkeManager | null
+
+// vai ser uma classe que vai gerenciar nosso WORKER
+
+export class TimerWorkeManager {
+    private worker: Worker;
+
+    private constructor(){
+        this.worker = new Worker(new URL('./timerWorker.js', import.meta.url));
+    }
+
+    static getInstance(){
+        if(!instance){
+            instance = new TimerWorkeManager()
+        }
+        return instance;
+    }
+
+    // CRIA A MENSAGEM PARA VER SE ESTA EM ANDAMENTO OU NÃO
+    postMessage(message: any){
+        this.worker.postMessage(message)
+    }
+
+    // MOSTRA EM AÇÃO PARA RODAR OU PARA
+    onmessage(callBack: (e: MessageEvent) => void){
+        this.worker.onmessage = callBack
+    }
+
+    // AQUI FINALIZA 
+    terminate(){
+        this.worker.terminate()
+        instance = null
+    }
+}
